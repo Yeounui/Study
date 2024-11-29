55e174f7c0cd19d87f36fa8a12519c0b81c9317d
Yeounui <Yeounui@gmail.com>
2024-11-29 21:33:09 +0900

    why you even cannot paraphrase

--- Self-studies/Cpp/Modoo code/Pointer4.c
+++ Self-studies/Cpp/Modoo code/Pointer4.c
@@ -3,7 +3,7 @@
 int main() {
 int a;
 int b;
-const int* pa = &a; // int 형 변수를 가리키는데 그 값을 절대로 바꾸지 말라
+const int* pa = &a; // int 형 변수를 가리키는 포인터를 다른 자료 형식으로 절대 바꾸지 말라
 // *pa = 3; // 올바르지 않은 문장
 pa = &b; // 올바른 문장
 return 0;
--- Self-studies/Cpp/Modoo code/Pointer5.c
+++ Self-studies/Cpp/Modoo code/Pointer5.c
@@ -3,7 +3,7 @@
 int main() {
 int a;
 int b;
-int* const pa = &a; //pa가 처음에 가리키는 것 (a) 말고 다른 것은 절대로 건드릴 수 없음
+int* const pa = &a; //pa가 처음에 가리키는 a 변수의 주소값을 절대 건드릴 수 없음
 *pa = 3; // 올바른 문장
 // pa = &b; // 올바르지 않은 문장
 return 0;
